e60dc294d3f21548e9c5df369f720ed0f722dbeb
Nitin Tiwari <128287787+thisisnitintiwarii@users.noreply.github.com>
2025-11-25 17:11:41 +0530

    silly fix

--- backend/socket.js
+++ backend/socket.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://vybe-gzu3.onrender.com/",
+    origin: "https://vybe-gzu3.onrender.com",
     methods: ["GET", "POST"],
   },
 });
