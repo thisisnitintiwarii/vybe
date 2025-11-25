034058dbbe040237aaff7fcb294250c0e9e95c76
Nitin Tiwari <128287787+thisisnitintiwarii@users.noreply.github.com>
2025-11-25 16:32:48 +0530

    secure edit

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -52,8 +52,8 @@ export const signUp = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 10 * 365 * 24 * 60 * 60 * 1000,
-      secure: false,
-      sameSite: "Strict",
+      secure: true,
+      sameSite: "None",
     });
 
     return res.status(200).json(user);
@@ -95,8 +95,8 @@ export const signIn = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 10 * 365 * 24 * 60 * 60 * 10,
-      secure: false,
-      sameSite: "Strict",
+      secure: true,
+      sameSite: "None",
     });
 
     return res.status(200).json(user);
